9ce845ab49f0a29b2c853096d09c3d5275ce7595
salexs95 <salexs95@yandex.ru>
2020-04-22 23:33:19 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[
-    ],
     python_requires='>=3.4',
 )
